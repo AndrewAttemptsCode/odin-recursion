d8eaf6bb0b373961bca4744232f854b5e152c119
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-09-07 17:56:14 +0100

    Add recursion version of the fibonacci function

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,5 @@
+// Iteration
+
 function fibIteration(num) {
   let a = 0;
   let b = 1;
@@ -20,3 +22,21 @@ function fibIteration(num) {
 const iteration = fibIteration(8);
 
 console.log(iteration);
+
+// Recursion
+
+function fibRecursion(num) {
+  if (num <= 0) return [];
+  if (num === 1) return [0];
+  if (num === 2) return [0, 1];
+
+  let sequence = fibRecursion(num - 1);
+
+  sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+
+  return sequence;
+}
+
+const recursion = fibRecursion(8);
+
+console.log(recursion);
